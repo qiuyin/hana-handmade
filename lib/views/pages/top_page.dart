85b67ec965024acbb36fa719a511cc73cac2c850
yin <yinjihua@gmail.com>
2021-11-07 13:41:31 +0900

    新商品一覧の枠のタイトルを追加

--- lib/views/pages/top_page.dart
+++ lib/views/pages/top_page.dart
@@ -13,34 +13,35 @@ class TopPage extends StatelessWidget {
 
   PreferredSizeWidget _appBar() {
     return PreferredSize(
-        preferredSize: Size.fromHeight(60),
-        child: Material(
-          color: colors.hanaPink,
-          elevation: 1,
-          child: Stack(
-            children: [
-              Center(
-                  child: SvgPicture.asset('images/logo.svg',
-                      semanticsLabel: 'Hana Logo')),
-              Center(
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    SvgPicture.asset('images/search.svg',
-                        semanticsLabel: 'search'),
-                    SizedBox(
-                      width: 8,
-                    ),
-                    SvgPicture.asset('images/cart.svg', semanticsLabel: 'Cart'),
-                    SizedBox(
-                      width: 18,
-                    )
-                  ],
-                ),
-              )
-            ],
-          ),
-        ));
+      preferredSize: Size.fromHeight(60),
+      child: Material(
+        color: colors.hanaPink,
+        elevation: 1,
+        child: Stack(
+          children: [
+            Center(
+                child: SvgPicture.asset('images/logo.svg',
+                    semanticsLabel: 'Hana Logo')),
+            Center(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  SvgPicture.asset('images/search.svg',
+                      semanticsLabel: 'search'),
+                  SizedBox(
+                    width: 8,
+                  ),
+                  SvgPicture.asset('images/cart.svg', semanticsLabel: 'Cart'),
+                  SizedBox(
+                    width: 18,
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 
   Widget _body(BuildContext context) {
@@ -49,7 +50,33 @@ class TopPage extends StatelessWidget {
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
           Text('画像'),
-          Text('NEW PRODUCTS'),
+          Row(
+            children: [
+              const Expanded(
+                child: Divider(
+                  thickness: 0.3,
+                  color: colors.darkGray,
+                  indent: 24,
+                  endIndent: 24,
+                ),
+              ),
+              SelectableText(
+                '新商品',
+                strutStyle: StrutStyle(height: 1.5),
+                style: TextStyle(
+                    fontWeight: FontWeight.bold, fontStyle: FontStyle.italic),
+              ),
+              SizedBox(height: 10),
+              const Expanded(
+                child: Divider(
+                  thickness: 0.3,
+                  color: colors.darkGray,
+                  indent: 24,
+                  endIndent: 24,
+                ),
+              ),
+            ],
+          ),
           Text('新商品画像リスト'),
           Text('カテゴリ'),
         ],
