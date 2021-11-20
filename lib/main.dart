61b3622c77e43f3c273923e0c020bd0ca21e08b0
jihua yin <yinjihua@gmail.com>
2021-11-20 12:23:39 +0900

    title更新

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Hana handmade',
+      title: 'HANA handmade',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
