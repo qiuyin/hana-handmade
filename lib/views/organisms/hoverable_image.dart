d346e70db89db97e425b765388f201464d1ccb34
jihua yin <yinjihua@gmail.com>
2022-01-08 10:26:55 +0900

    「HANA-1」商品一覧の商品画像をホバーするときに画像を変える

---
+++ lib/views/organisms/hoverable_image.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+
+class HoverableImage extends StatefulWidget {
+  final String imageUrl1;
+  final String imageUrl2;
+  const HoverableImage({this.imageUrl1, this.imageUrl2});
+
+  @override
+  _HoverableImageState createState() => _HoverableImageState();
+}
+
+class _HoverableImageState extends State<HoverableImage> {
+  String image;
+
+  @override
+  void initState() {
+    image = widget.imageUrl1;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MouseRegion(
+      onHover: (PointerEvent details) {
+        setState(() {
+          image = widget.imageUrl2;
+        });
+      },
+      onExit: (PointerExitEvent details) {
+        setState(() {
+          image = widget.imageUrl1;
+        });
+      },
+      child: Image.asset(
+        image,
+        fit: BoxFit.contain,
+      ),
+    );
+  }
+}
