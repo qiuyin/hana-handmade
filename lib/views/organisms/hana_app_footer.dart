567f177bedb3ce7cf39166dd75fbb963534c32ac
hanachan3 <jinyinhua3@gmail.com>
2022-01-22 12:36:34 +0900

    footerの　デザイン変更

--- lib/views/organisms/hana_app_footer.dart
+++ lib/views/organisms/hana_app_footer.dart
@@ -1,18 +1,21 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:hana_handmade/common/colors.dart' as colors;
 
 class HanaAppFooter extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
       padding: EdgeInsets.only(left: 70, right: 70),
-      height: 200,
-      color: Colors.white,
+      height: 120,
+      color: colors.hanaPink.withOpacity(0.66),
       child: Row(
         children: [
           Expanded(
-            flex: 8,
+            flex: 1,
             child: SvgPicture.asset(
               'images/logo.svg',
               semanticsLabel: 'Hana Logo',
@@ -20,25 +23,34 @@ class HanaAppFooter extends StatelessWidget {
             ),
           ),
           Expanded(
-            flex: 1,
+            flex: 8,
             child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisAlignment: MainAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.end,
               children: [
-                Text(
-                  'FOLLOW US',
-                ),
-                SizedBox(height: 10),
                 Row(
-                  mainAxisAlignment: MainAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                   children: [
-                    Image.asset('images/SNS_list/youtube.png'),
-                    SizedBox(width: 10),
-                    Image.asset('images/SNS_list/Instagram.png'),
-                    SizedBox(width: 10),
-                    Image.asset('images/SNS_list/g-mail.png'),
+                    SizedBox(width: 40),
+                    Text("・配送について"),
+                    Text("・返品交換について"),
+                    Text("・支払について"),
+                    SizedBox(width: 40),
                   ],
-                )
+                ),
+                SizedBox(height: 25),
+              ],
+            ),
+          ),
+          Expanded(
+            flex: 1,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                FaIcon(FontAwesomeIcons.youtube),
+                SizedBox(width: 10),
+                FaIcon(FontAwesomeIcons.instagram),
+                SizedBox(width: 10),
+                FaIcon(FontAwesomeIcons.line),
               ],
             ),
           ),
